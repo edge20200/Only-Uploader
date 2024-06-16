21a956198480fb4e2be420b23c35eb57f766c03c
edge20200 <126633394+edge20200@users.noreply.github.com>
2024-06-16 19:01:11 -0400

    Update RF.py

--- src/trackers/RF.py
+++ src/trackers/RF.py
@@ -27,7 +27,7 @@ class RF():
         self.source_flag = 'ReelFliX'
         self.upload_url = 'https://reelflix.xyz/api/torrents/upload'
         self.search_url = 'https://reelflix.xyz/api/torrents/filter'
-        self.forum_link = "\n[center][url=https://github.com/edge20200/UploadAssistant]Created by Upload Assistant[/url][/center]"
+        self.forum_link = "\n[center][url=https://github.com/edge20200/Only-Uploader]Powered by Only-Uploader[/url][/center]"
         self.banned_groups = [""]
         pass
 
