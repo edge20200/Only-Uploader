079aac458582090310329e2cc6c083435715a4e8
edge20200 <126633394+edge20200@users.noreply.github.com>
2026-02-14 23:24:44 -0500

    Refactor OTW class and update method parameters

--- src/trackers/OTW.py
+++ src/trackers/OTW.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 # import discord
 import asyncio
+import glob
+import os
+import platform
+
+import bencodepy
 import requests
 from str2bool import str2bool
-import platform
-import bencodepy
-import os
-import glob
 
-from src.trackers.COMMON import COMMON
 from src.console import console
+from src.trackers.COMMON import COMMON
 
 
-class OTW():
+class OTW:
     """
     Edit for Tracker:
         Edit BASE.torrent with announce and source
@@ -23,128 +24,176 @@ class OTW():
 
     def __init__(self, config):
         self.config = config
-        self.tracker = 'OTW'
-        self.source_flag = 'OTW'
-        self.upload_url = 'https://oldtoons.world/api/torrents/upload'
-        self.search_url = 'https://oldtoons.world/api/torrents/filter'
+        self.tracker = "OTW"
+        self.source_flag = "OTW"
+        self.upload_url = "https://oldtoons.world/api/torrents/upload"
+        self.search_url = "https://oldtoons.world/api/torrents/filter"
+        self.torrent_url = "https://oldtoons.world/api/torrents/"
         self.signature = "\n[center][url=https://github.com/edge20200/Only-Uploader]Powered by Only-Uploader[/url][/center]"
         self.banned_groups = [""]
         pass
 
     async def get_cat_id(self, category_name):
         category_id = {
-            'MOVIE': '1',
-            'TV': '2',
-        }.get(category_name, '0')
+            "MOVIE": "1",
+            "TV": "2",
+        }.get(category_name, "0")
         return category_id
 
     async def get_type_id(self, type):
         type_id = {
-            'DISC': '1',
-            'REMUX': '2',
-            'WEBDL': '4',
-            'WEBRIP': '5',
-            'HDTV': '6',
-            'ENCODE': '3'
-        }.get(type, '0')
+            "DISC": "1",
+            "REMUX": "2",
+            "WEBDL": "4",
+            "WEBRIP": "5",
+            "HDTV": "6",
+            "ENCODE": "3",
+        }.get(type, "0")
         return type_id
 
     async def get_res_id(self, resolution):
         resolution_id = {
-            '8640p': '10',
-            '4320p': '1',
-            '2160p': '2',
-            '1440p': '3',
-            '1080p': '3',
-            '1080i': '4',
-            '720p': '5',
-            '576p': '6',
-            '576i': '7',
-            '480p': '8',
-            '480i': '9'
-        }.get(resolution, '10')
+            "8640p": "10",
+            "4320p": "1",
+            "2160p": "2",
+            "1440p": "3",
+            "1080p": "3",
+            "1080i": "4",
+            "720p": "5",
+            "576p": "6",
+            "576i": "7",
+            "480p": "8",
+            "480i": "9",
+        }.get(resolution, "10")
         return resolution_id
 
     async def upload(self, meta, disctype):
         common = COMMON(config=self.config)
         await common.edit_torrent(meta, self.tracker, self.source_flag)
-        cat_id = await self.get_cat_id(meta['category'])
-        type_id = await self.get_type_id(meta['type'])
-        resolution_id = await self.get_res_id(meta['resolution'])
+        cat_id = await self.get_cat_id(meta["category"])
+        type_id = await self.get_type_id(meta["type"])
+        resolution_id = await self.get_res_id(meta["resolution"])
         await common.unit3d_edit_desc(meta, self.tracker, self.signature)
-        region_id = await common.unit3d_region_ids(meta.get('region'))
-        distributor_id = await common.unit3d_distributor_ids(meta.get('distributor'))
-        if meta['anon'] == 0 and bool(str2bool(str(self.config['TRACKERS'][self.tracker].get('anon', "False")))) is False:
+        region_id = await common.unit3d_region_ids(meta.get("region"))
+        distributor_id = await common.unit3d_distributor_ids(meta.get("distributor"))
+        if (
+            meta["anon"] == 0
+            and bool(
+                str2bool(
+                    str(self.config["TRACKERS"][self.tracker].get("anon", "False"))
+                )
+            )
+            is False
+        ):
             anon = 0
         else:
             anon = 1
 
-        if meta['bdinfo'] is not None:
+        if meta["bdinfo"] is not None:
             mi_dump = None
-            bd_dump = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/BD_SUMMARY_00.txt", 'r', encoding='utf-8').read()
+            bd_dump = open(
+                f"{meta['base_dir']}/tmp/{meta['uuid']}/BD_SUMMARY_00.txt",
+                "r",
+                encoding="utf-8",
+            ).read()
         else:
-            mi_dump = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/MEDIAINFO.txt", 'r', encoding='utf-8').read()
+            mi_dump = open(
+                f"{meta['base_dir']}/tmp/{meta['uuid']}/MEDIAINFO.txt",
+                "r",
+                encoding="utf-8",
+            ).read()
             bd_dump = None
-        desc = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/[{self.tracker}]DESCRIPTION.txt", 'r', encoding='utf-8').read()
-        open_torrent = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/[{self.tracker}]{meta['clean_name']}.torrent", 'rb')
-        files = {'torrent': open_torrent}
-        base_dir = meta['base_dir']
-        uuid = meta['uuid']
+        desc = open(
+            f"{meta['base_dir']}/tmp/{meta['uuid']}/[{self.tracker}]DESCRIPTION.txt",
+            "r",
+            encoding="utf-8",
+        ).read()
+        open_torrent = open(
+            f"{meta['base_dir']}/tmp/{meta['uuid']}/[{self.tracker}]{meta['clean_name']}.torrent",
+            "rb",
+        )
+        files = {"torrent": open_torrent}
+        base_dir = meta["base_dir"]
+        uuid = meta["uuid"]
         specified_dir_path = os.path.join(base_dir, "tmp", uuid, "*.nfo")
         nfo_files = glob.glob(specified_dir_path)
         nfo_file = None
         if nfo_files:
-            nfo_file = open(nfo_files[0], 'rb')
+            nfo_file = open(nfo_files[0], "rb")
         if nfo_file:
-            files['nfo'] = ("nfo_file.nfo", nfo_file, "text/plain")
+            files["nfo"] = ("nfo_file.nfo", nfo_file, "text/plain")
         data = {
-            'name': meta['name'],
-            'description': desc,
-            'mediainfo': mi_dump,
-            'bdinfo': bd_dump,
-            'category_id': cat_id,
-            'type_id': type_id,
-            'resolution_id': resolution_id,
-            'tmdb': meta['tmdb'],
-            'imdb': meta['imdb_id'].replace('tt', ''),
-            'tvdb': meta['tvdb_id'],
-            'mal': meta['mal_id'],
-            'igdb': 0,
-            'anonymous': anon,
-            'stream': meta['stream'],
-            'sd': meta['sd'],
-            'keywords': meta['keywords'],
-            'personal_release': int(meta.get('personalrelease', False)),
-            'internal': 0,
-            'featured': 0,
-            'free': 0,
-            'doubleup': 0,
-            'sticky': 0,
+            "name": meta["name"],
+            "description": desc,
+            "mediainfo": mi_dump,
+            "bdinfo": bd_dump,
+            "category_id": cat_id,
+            "type_id": type_id,
+            "resolution_id": resolution_id,
+            "tmdb": meta["tmdb"],
+            "imdb": meta["imdb_id"].replace("tt", ""),
+            "tvdb": meta["tvdb_id"],
+            "mal": meta["mal_id"],
+            "igdb": 0,
+            "anonymous": anon,
+            "stream": meta["stream"],
+            "sd": meta["sd"],
+            "keywords": meta["keywords"],
+            "personal_release": int(meta.get("personalrelease", False)),
+            "internal": 0,
+            "featured": 0,
+            "free": 0,
+            "doubleup": 0,
+            "sticky": 0,
         }
         # Internal
-        if self.config['TRACKERS'][self.tracker].get('internal', False) is True:
-            if meta['tag'] != "" and (meta['tag'][1:] in self.config['TRACKERS'][self.tracker].get('internal_groups', [])):
-                data['internal'] = 1
+        if self.config["TRACKERS"][self.tracker].get("internal", False) is True:
+            if meta["tag"] != "" and (
+                meta["tag"][1:]
+                in self.config["TRACKERS"][self.tracker].get("internal_groups", [])
+            ):
+                data["internal"] = 1
 
         if region_id != 0:
-            data['region_id'] = region_id
+            data["region_id"] = region_id
         if distributor_id != 0:
-            data['distributor_id'] = distributor_id
-        if meta.get('category') == "TV":
-            data['season_number'] = meta.get('season_int', '0')
-            data['episode_number'] = meta.get('episode_int', '0')
+            data["distributor_id"] = distributor_id
+        if meta.get("category") == "TV":
+            data["season_number"] = meta.get("season_int", "0")
+            data["episode_number"] = meta.get("episode_int", "0")
         headers = {
-            'User-Agent': f'Upload Assistant/2.2 ({platform.system()} {platform.release()})'
+            "User-Agent": f"Upload Assistant/2.2 ({platform.system()} {platform.release()})"
         }
-        params = {
-            'api_token': self.config['TRACKERS'][self.tracker]['api_key'].strip()
-        }
+        params = {"api_token": self.config["TRACKERS"][self.tracker]["api_key"].strip()}
 
-        if meta['debug'] is False:
-            response = requests.post(url=self.upload_url, files=files, data=data, headers=headers, params=params)
+        if meta["debug"] is False:
+            response = requests.post(
+                url=self.upload_url,
+                files=files,
+                data=data,
+                headers=headers,
+                params=params,
+            )
             try:
-                console.print(response.json())
-            except Exception:
+                resp_json = response.json()
+                console.print(resp_json)
+
+                # 🔥 Download the torrent file after upload
+                if "data" in resp_json and resp_json["data"]:
+                    await common.add_tracker_torrent(
+                        meta,
+                        self.tracker,
+                        self.source_flag,
+                        self.config["TRACKERS"][self.tracker].get("announce_url"),
+                        "https://oldtoons.world/torrents/" + str(resp_json["data"]),
+                        headers=headers,
+                        params=params,
+                        downurl=resp_json["data"],
+                    )
+            except Exception as e:
+                console.print(
+                    f"[red]Error while uploading or downloading torrent: {e}[/red]"
+                )
                 console.print("It may have uploaded, go check")
                 return
         else:
@@ -156,37 +205,39 @@ class OTW():
         dupes = []
         console.print("[yellow]Searching for existing torrents on site...")
         params = {
-            'api_token': self.config['TRACKERS'][self.tracker]['api_key'].strip(),
-            'tmdbId': meta['tmdb'],
-            'categories[]': await self.get_cat_id(meta['category']),
-            'types[]': await self.get_type_id(meta['type']),
-            'resolutions[]': await self.get_res_id(meta['resolution']),
-            'name': ""
+            "api_token": self.config["TRACKERS"][self.tracker]["api_key"].strip(),
+            "tmdbId": meta["tmdb"],
+            "categories[]": await self.get_cat_id(meta["category"]),
+            "types[]": await self.get_type_id(meta["type"]),
+            "resolutions[]": await self.get_res_id(meta["resolution"]),
+            "name": "",
         }
-        if meta.get('edition', "") != "":
-            params['name'] = params['name'] + f" {meta['edition']}"
+        if meta.get("edition", "") != "":
+            params["name"] = params["name"] + f" {meta['edition']}"
         try:
             response = requests.get(url=self.search_url, params=params)
             response = response.json()
-            for each in response['data']:
-                result = [each][0]['attributes']['name']
+            for each in response["data"]:
+                result = [each][0]["attributes"]["name"]
                 # difference = SequenceMatcher(None, meta['clean_name'], result).ratio()
                 # if difference >= 0.05:
                 dupes.append(result)
         except Exception:
-            console.print('[bold red]Unable to search for existing torrents on site. Either the site is down or your API key is incorrect')
+            console.print(
+                "[bold red]Unable to search for existing torrents on site. Either the site is down or your API key is incorrect"
+            )
             await asyncio.sleep(5)
 
         return dupes
 
     async def search_torrent_page(self, meta, disctype):
         torrent_file_path = f"{meta['base_dir']}/tmp/{meta['uuid']}/[{self.tracker}]{meta['clean_name']}.torrent"
-        Name = meta['name']
+        Name = meta["name"]
         quoted_name = f'"{Name}"'
 
         params = {
-            'api_token': self.config['TRACKERS'][self.tracker]['api_key'].strip(),
-            'name': quoted_name
+            "api_token": self.config["TRACKERS"][self.tracker]["api_key"].strip(),
+            "name": quoted_name,
         }
 
         try:
@@ -194,18 +245,20 @@ class OTW():
             response.raise_for_status()
             response_data = response.json()
 
-            if response_data['data'] and isinstance(response_data['data'], list):
-                details_link = response_data['data'][0]['attributes'].get('details_link')
+            if response_data["data"] and isinstance(response_data["data"], list):
+                details_link = response_data["data"][0]["attributes"].get(
+                    "details_link"
+                )
 
                 if details_link:
-                    with open(torrent_file_path, 'rb') as open_torrent:
+                    with open(torrent_file_path, "rb") as open_torrent:
                         torrent_data = open_torrent.read()
 
                     torrent = bencodepy.decode(torrent_data)
-                    torrent[b'comment'] = details_link.encode('utf-8')
+                    torrent[b"comment"] = details_link.encode("utf-8")
                     updated_torrent_data = bencodepy.encode(torrent)
 
-                    with open(torrent_file_path, 'wb') as updated_torrent_file:
+                    with open(torrent_file_path, "wb") as updated_torrent_file:
                         updated_torrent_file.write(updated_torrent_data)
 
                     return details_link
