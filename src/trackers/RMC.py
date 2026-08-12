fac973116ab8ece08537657d42756c78f43434f6
edge20200 <126633394+edge20200@users.noreply.github.com>
2026-08-12 19:35:04 -0400

    Add RMC tracker implementation

---
+++ src/trackers/RMC.py
@@ -0,0 +1,256 @@
+# -*- coding: utf-8 -*-
+# import discord
+import asyncio
+import requests
+import platform
+import re
+from str2bool import str2bool
+import bencodepy
+import os
+import glob
+
+from src.trackers.COMMON import COMMON
+from src.console import console
+
+
+class RMC():
+    """
+    Edit for Tracker:
+        Edit BASE.torrent with announce and source
+        Check for duplicates
+        Set type/category IDs
+        Upload
+    """
+
+    def __init__(self, config):
+        self.config = config
+        self.tracker = 'RMC'
+        self.source_flag = 'RMC'
+        self.upload_url = 'https://retro-movies.club/api/torrents/upload'
+        self.search_url = 'https://retro-movies.club/api/torrents/filter'
+        self.signature = "\n[center][url=https://github.com/edge20200/Only-Uploader]Powered by Only-Uploader[/url][/center]"
+        self.banned_groups = ["Oj", "3LTON", "4yEo", "ADE", "AFG", "AniHLS", "AnimeRG", "AniURL", "AROMA", "aXXo", "CM8", "CrEwSaDe", "DeadFish", "DNL", "ELiTE", "eSc", "FaNGDiNG0", "Flights", "FRDS", "FUM", "GalaxyRG", "HAiKU", "HDS", "HDTime", "INFINITY", "ION10", "iPlanet", "JIVE", "KiNGDOM", "LAMA", "Leffe", "LOAD", "mHD", "nHD", "NOIVTC", "nSD", "RARBG", "RDN", "REsuRRecTioN", "RMTeam", "SANTi", "SicFoI", "SPASM", "STUTTERSHIT", "Telly", "TM", "UPiNSMOKE", "WORLD", "WAF", "xRed", "XS", "YELLO", "YIFY", "YTS", "YTSMX", "ZKBL", "ZmN"]
+        pass
+
+    async def upload(self, meta, disctype):
+        common = COMMON(config=self.config)
+        await common.edit_torrent(meta, self.tracker, self.source_flag)
+        await common.unit3d_edit_desc(meta, self.tracker, self.signature, comparison=True)
+        region_id = await common.unit3d_region_ids(meta.get('region'))
+        distributor_id = await common.unit3d_distributor_ids(meta.get('distributor'))
+        cat_id = await self.get_cat_id(meta['category'])
+        type_id = await self.get_type_id(meta['type'])
+        resolution_id = await self.get_res_id(meta['resolution'])
+        rmc_name = await self.edit_name(meta)
+        if meta['anon'] == 0 and bool(str2bool(str(self.config['TRACKERS'][self.tracker].get('anon', "False")))) is False:
+            anon = 0
+        else:
+            anon = 1
+        if meta['bdinfo'] is not None:
+            mi_dump = None
+            bd_dump = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/BD_SUMMARY_00.txt", 'r', encoding='utf-8').read()
+        else:
+            mi_dump = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/MEDIAINFO.txt", 'r', encoding='utf-8').read()
+            bd_dump = None
+        desc = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/[{self.tracker}]DESCRIPTION.txt", 'r', encoding='utf-8').read()
+        open_torrent = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/[{self.tracker}]{meta['clean_name']}.torrent", 'rb')
+        files = {'torrent': open_torrent}
+        base_dir = meta['base_dir']
+        uuid = meta['uuid']
+        specified_dir_path = os.path.join(base_dir, "tmp", uuid, "*.nfo")
+        nfo_files = glob.glob(specified_dir_path)
+        nfo_file = None
+        if nfo_files:
+            nfo_file = open(nfo_files[0], 'rb')
+        if nfo_file:
+            files['nfo'] = ("nfo_file.nfo", nfo_file, "text/plain")
+        data = {
+            'name': rmc_name,
+            'description': desc,
+            'mediainfo': mi_dump,
+            'bdinfo': bd_dump,
+            'category_id': cat_id,
+            'type_id': type_id,
+            'resolution_id': resolution_id,
+            'tmdb': meta['tmdb'],
+            'imdb': meta['imdb_id'].replace('tt', ''),
+            'tvdb': meta['tvdb_id'],
+            'mal': meta['mal_id'],
+            'igdb': 0,
+            'anonymous': anon,
+            'stream': meta['stream'],
+            'sd': meta['sd'],
+            'keywords': meta['keywords'],
+            'personal_release': int(meta.get('personalrelease', False)),
+            'internal': 0,
+            'featured': 0,
+            'free': 0,
+            'doubleup': 0,
+            'sticky': 0,
+        }
+        # Internal
+        if self.config['TRACKERS'][self.tracker].get('internal', False) is True:
+            if meta['tag'] != "" and (meta['tag'][1:] in self.config['TRACKERS'][self.tracker].get('internal_groups', [])):
+                data['internal'] = 1
+
+        if region_id != 0:
+            data['region_id'] = region_id
+        if distributor_id != 0:
+            data['distributor_id'] = distributor_id
+        headers = {
+            'User-Agent': f'Upload Assistant/2.2 ({platform.system()} {platform.release()})'
+        }
+        params = {
+            'api_token': self.config['TRACKERS'][self.tracker]['api_key'].strip()
+        }
+        if meta['debug'] is False:
+            response = requests.post(url=self.upload_url, files=files, data=data, headers=headers, params=params)
+            try:
+                resp_json = response.json()
+                console.print(resp_json)
+
+                # 🔥 Download the torrent file after upload
+                if "data" in resp_json and resp_json["data"]:
+                    await common.add_tracker_torrent(
+                        meta,
+                        self.tracker,
+                        self.source_flag,
+                        self.config['TRACKERS'][self.tracker].get('announce_url'),
+                        "https://retro-movies.club/torrents/" + str(resp_json["data"]),
+                        headers=headers,
+                        params=params,
+                        downurl=resp_json["data"]
+                    )
+            except Exception as e:
+                console.print(f"[red]Error while uploading or downloading torrent: {e}[/red]")
+                console.print("It may have uploaded, go check")
+                return
+        else:
+            console.print("[cyan]Request Data:")
+            console.print(data)
+        open_torrent.close()
+
+    async def edit_name(self, meta):
+        rmc_name = meta['name']
+        tag_lower = meta['tag'].lower()
+        invalid_tags = ["nogrp", "nogroup", "unknown", "-unk-"]
+
+        if meta['tag'] == "" or any(invalid_tag in tag_lower for invalid_tag in invalid_tags):
+            for invalid_tag in invalid_tags:
+                rmc_name = re.sub(f"-{invalid_tag}", "", rmc_name, flags=re.IGNORECASE)
+            rmc_name = f"{rmc_name}-NoGroup"
+
+        return rmc_name
+
+    async def get_cat_id(self, category_name):
+        category_id = {
+            'MOVIE': '1',
+        }.get(category_name, '0')
+        return category_id
+
+    async def get_type_id(self, type):
+        type_id = {
+            'DISC': '1',
+            'REMUX': '2',
+            'WEBDL': '7',
+            'WEBRIP': '8',
+            # 'FANRES': '6',
+            'ENCODE': '5',
+            'HDTV': '10',
+        }.get(type, '0')
+        return type_id
+
+    async def get_res_id(self, resolution):
+        resolution_id = {
+            # '8640p':'10',
+            '4320p': '1',
+            '2160p': '2',
+            # '1440p' : '3',
+            '1080p': '3',
+            '1080i': '4',
+            '720p': '5',
+            '576p': '6',
+            '576i': '7',
+            '480p': '8',
+            '480i': '9'
+        }.get(resolution, '10')
+        return resolution_id
+
+    async def search_existing(self, meta, disctype):
+        disallowed_keywords = {'XXX', 'Erotic'}
+        if any(keyword in meta['keywords'] for keyword in disallowed_keywords):
+            console.print('[bold red]Erotic not allowed.')
+            meta['skipping'] = "RMC"
+            return
+        if meta.get('category') == "TV":
+            console.print('[bold red]This site only ALLOWS Movies.')
+            meta['skipping'] = "RMC"
+            return
+        if meta.get('year', 0) > 2000:
+            console.print('[bold red]Only movies from year 2000 or older are allowed.')
+            meta['skipping'] = "RMC"
+            return
+        dupes = []
+        console.print("[yellow]Searching for existing torrents on site...")
+        params = {
+            'api_token': self.config['TRACKERS'][self.tracker]['api_key'].strip(),
+            'tmdbId': meta['tmdb'],
+            'categories[]': await self.get_cat_id(meta['category']),
+            'types[]': await self.get_type_id(meta['type']),
+            'resolutions[]': await self.get_res_id(meta['resolution']),
+            'name': ""
+        }
+        if meta.get('edition', "") != "":
+            params['name'] = params['name'] + meta['edition']
+        try:
+            response = requests.get(url=self.search_url, params=params)
+            response = response.json()
+            for each in response['data']:
+                result = [each][0]['attributes']['name']
+                # difference = SequenceMatcher(None, meta['clean_name'], result).ratio()
+                # if difference >= 0.05:
+                dupes.append(result)
+        except Exception:
+            console.print('[bold red]Unable to search for existing torrents on site. Either the site is down or your API key is incorrect')
+            await asyncio.sleep(5)
+
+        return dupes
+
+    async def search_torrent_page(self, meta, disctype):
+        torrent_file_path = f"{meta['base_dir']}/tmp/{meta['uuid']}/[{self.tracker}]{meta['clean_name']}.torrent"
+        Name = meta['name']
+        quoted_name = f'"{Name}"'
+
+        params = {
+            'api_token': self.config['TRACKERS'][self.tracker]['api_key'].strip(),
+            'name': quoted_name
+        }
+
+        try:
+            response = requests.get(url=self.search_url, params=params)
+            response.raise_for_status()
+            response_data = response.json()
+
+            if response_data['data'] and isinstance(response_data['data'], list):
+                details_link = response_data['data'][0]['attributes'].get('details_link')
+
+                if details_link:
+                    with open(torrent_file_path, 'rb') as open_torrent:
+                        torrent_data = open_torrent.read()
+
+                    torrent = bencodepy.decode(torrent_data)
+                    torrent[b'comment'] = details_link.encode('utf-8')
+                    updated_torrent_data = bencodepy.encode(torrent)
+
+                    with open(torrent_file_path, 'wb') as updated_torrent_file:
+                        updated_torrent_file.write(updated_torrent_data)
+
+                    return details_link
+                else:
+                    return None
+            else:
+                return None
+
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred during the request: {e}")
+            return None
